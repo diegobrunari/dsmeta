b64dd41c70f4931335e326ffbe70fafa403fb5c9
Diego Brunari <diego.skp@hotmail.comgit config --global user.email diego.skp@hotmail.com>
2022-07-12 02:03:34 -0300

    First component

---
+++ frontend/src/components/NotificationBotton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationBotton() {
+    return (
+        <div className="dsmeta-red-botton">
+            <img src={icon} alt="Imagem" />
+        </div>
+    )
+}
+
+export default NotificationBotton
