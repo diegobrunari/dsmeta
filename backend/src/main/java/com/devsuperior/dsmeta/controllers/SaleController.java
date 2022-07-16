0069fa0dacf4c13f5db58787af6459c3408821b5
Diego Brunari <diego.skp@hotmail.comgit config --global user.email diego.skp@hotmail.com>
2022-07-16 03:53:23 -0300

    Date update

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -19,19 +19,20 @@ public class SaleController {
 	
 	@Autowired
 	private SaleService service;
+	
 	@Autowired
 	private SmsService smsService;
-	
+
 	@GetMapping
 	public Page<Sale> findSales(
-			@RequestParam(value="minDate", defaultValue = "") String minDate, 
-			@RequestParam(value="max Date", defaultValue = "") String maxDate,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
 			Pageable pageable) {
-		
-		return service.findSales(minDate, maxDate, pageable); 
-	} 
+		return service.findSales(minDate, maxDate, pageable);
+	}
+	
 	@GetMapping("/{id}/notification")
 	public void notifySms(@PathVariable Long id) {
-		smsService.sendSms(id);
+		smsService.sendSms(id);		
 	}
 }
